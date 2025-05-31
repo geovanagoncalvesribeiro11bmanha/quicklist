55cc478c3cfaeed0673d69eae19f0dfe1b6d474f
Geovana Goncalves <geovanagoncalvesribeiro@eaportal.org>
2025-05-31 16:59:00 -0300

    criação da função showItemsList() para exibir os itens dentro da section.list

--- main.js
+++ main.js
@@ -11,4 +11,30 @@ function addItem(){
     items.push(item)
 
     document.querySelector("#item").value = ""
+
+    showItemsList()
+}
+
+function showItemsList (){
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent =""
+
+    items.map((item, index) => {
+        sectionList.innerHTML += `
+         <div class="item">
+                <div>
+                    <input type="checkbox" name="list" id="item-${index}">
+                    <div class="custom-checkbox">
+                        <img src="./assets-20250531T174048Z-1-001/assets/checked.svg" alt="imagem do check">
+                    </div>
+                    <label for="${index}">${item.name}</label>
+                </div>
+
+                <button>
+                    <img src="./assets-20250531T174048Z-1-001/assets/trash-icon.svg" alt="trash icon">
+                </button>
+            </div>
+            
+        `
+    })
 }
